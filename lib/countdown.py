b15eb9660682c72668af3fcccce33586d8fb2708
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-09-28 20:46:20 +0300

    Countdown to Midnight

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+def countdown(int):
+    count = int
+    while count >= 1:
+        print (f'{count} SECOND(S)!')
+        count -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(int):
+    count = int
+    while count >= 1:
+        time.sleep(1)
+        print (f'{count} SECOND(S)!')
+        count -= 1
+    print("HAPPY NEW YEAR!")
